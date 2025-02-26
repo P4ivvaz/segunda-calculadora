22ff2becc2ce100416398ad5ae2d303978e56312
P4ivvaz <livia.psousa@senacsp.edu.br>
2025-02-26 20:52:10 -0300

    Adicionar arquivos de projeto.

---
+++ segunda-calculadora/Program.cs
@@ -0,0 +1,25 @@
+﻿string valor;
+do
+    Console.WriteLine("digite o primeiro número"); while (true);
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("digite o segundo número:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("escolha a operação(/,+,-,*):");
+string symbol = Console.ReadLine();
+switch (symbol)
+{
+    case "*":
+        valor = n1 * n2;
+        Console.WriteLine("Multiplication:" + valor);
+        break;
+      
+    case "/":
+        valor = n1 / n2;
+        Console.WriteLine("Division:" + valor);
+        break;
+        Console.WriteLine("Deseja continuar?");
+        break;
+
+
+
+}
